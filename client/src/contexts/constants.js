5713443d4a27f555b92daaf1f6114f8651b7b109
Lưu Công Quang Vũ <tieungpk1@gmail.com>
2021-10-10 12:11:36 +0700

    prepare deloy

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,5 +1,5 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5000/api"
-    : "some deploy Url";
+    : "https://lit-taiga-09850.herokuapp.com/api";
 export const LOCAL_STORAGE_TOKEN_NAME='learn-mern';
